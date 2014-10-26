2bd68e3e09f71d640d6c5b27079639c776bb8274
psmyrdek <przemek.smyrdek@gmail.com>
2014-10-26 19:02:21 +0100

    Projekt testów jednostkowych przy użyciu biblioteki Moq

---
+++ ResetAth/ResetAth.Test/Helpers/TestHelper.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using ResetAth.AutofacMvc.DAL.Contract;
+using ResetAth.AutofacMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResetAth.Test.Helpers
+{
+    public static class TestHelper
+    {
+        public static IPostRepository GetPostRepository()
+        {
+            IEnumerable<Post> AllPosts = new List<Post>
+            {
+                new Post { Id = 0, Title = "Post 1 Title", Content = "Some content", PathToImage = "1 ImagePath"},
+                new Post { Id = 1, Title = "Post 2 Title", Content = "Some content", PathToImage = "2 ImagePath"},
+                new Post { Id = 2, Title = "Post 3 Title", Content = "Some content", PathToImage = "3 ImagePath"}
+            };
+
+            var mock = new Mock<IPostRepository>();
+
+            mock.Setup(m => m.GetAll()).Returns(AllPosts);
+
+            mock.Setup(m => m.GetById(It.IsInRange<int>(0, 2, Range.Inclusive)))
+                .Returns((int i) => AllPosts.Single(x => x.Id == i));
+
+            return mock.Object;
+        }
+    }
+}
---
+++ ResetAth/ResetAth.Test/PostControllersTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Moq;
+using ResetAth.AutofacMvc.Controllers;
+using ResetAth.AutofacMvc.DAL.Contract;
+using ResetAth.AutofacMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResetAth.Test.Helpers;
+
+namespace ResetAth.Test
+{
+    [TestClass]
+    public class PostControllersTests
+    {
+        [TestMethod]
+        public void IndexMethodShouldReturnAllPosts()
+        {
+            //Arrange
+            IPostRepository repo = TestHelper.GetPostRepository();
+            PostController controller = new PostController(repo);
+
+            //Act
+            var result = controller.Index() as ViewResult;
+            IEnumerable<Post> posts = result.Model as IEnumerable<Post>;
+
+            //Assert
+            Assert.IsTrue(posts.Count() == 3);
+        }
+
+        [TestMethod]
+        public void SingleMethodShouldReturnCorrectPost()
+        {
+            //Arrange
+            IPostRepository repo = TestHelper.GetPostRepository();
+            PostController controller = new PostController(repo);
+
+            //Act
+            var result = controller.Single(1) as ViewResult;
+            Post post = result.Model as Post;
+
+            //Assert
+            Assert.IsTrue(post.Id == 1 && post.Title == "Post 2 Title");
+        }
+    }
+}
