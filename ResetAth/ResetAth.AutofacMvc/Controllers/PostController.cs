54ed723ebccdf4cedcf0aed6cc79641b95016ac2
psmyrdek <przemek.smyrdek@gmail.com>
2014-10-26 18:16:38 +0100

    Dodanie repozytorium dla MySql i PostController

---
+++ ResetAth/ResetAth.AutofacMvc/Controllers/PostController.cs
@@ -0,0 +1,49 @@
+﻿using ResetAth.AutofacMvc.DAL.Contract;
+using ResetAth.AutofacMvc.Models;
+using ResetAth.AutofacMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResetAth.AutofacMvc.Controllers
+{
+    public class PostController : Controller
+    {
+        private IPostRepository _postRepository;
+
+        public PostController(IPostRepository repo)
+        {
+            this._postRepository = repo;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Post> posts = this._postRepository.GetAll();
+            return View(posts);
+        }
+
+        public ActionResult Single(int id)
+        {
+            Post post = this._postRepository.GetById(id);
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult Add(PostViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this._postRepository.Add(new Post()
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    PathToImage = model.PathToImage
+                });
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ResetAth/ResetAth.AutofacMvc/DAL/Implementations/MySql/MySqlPostRepository.cs
@@ -0,0 +1,39 @@
+﻿using ResetAth.AutofacMvc.DAL.Contract;
+using ResetAth.AutofacMvc.Models;
+using Simple.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResetAth.AutofacMvc.DAL.Implementations.MySql
+{
+    public class MySqlPostRepository : IPostRepository
+    {
+        public IEnumerable<Models.Post> GetAll()
+        {
+            var db = Database.Open();
+            IEnumerable<Post> posts = db.post.All().Cast<Post>();
+            return posts;
+        }
+
+        public Models.Post GetById(int id)
+        {
+            var db = Database.Open();
+            Post post = db.post.FindById(id);
+            return post;
+        }
+
+        public void Add(Models.Post post)
+        {
+            var db = Database.Open();
+            db.post.Insert(Title: post.Title, Content: post.Content, PathToImage: post.PathToImage);
+        }
+
+        public void Delete(Models.Post post)
+        {
+            var db = Database.Open();
+            db.post.DeleteById(post.Id);
+        }
+    }
+}
